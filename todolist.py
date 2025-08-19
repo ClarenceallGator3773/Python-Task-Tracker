1cd43afa93ea335a694665e397d007f73cc09b6d
ClarenceallGator3773 <cartergoerges41@gmail.com>
2025-08-18 20:30:33 -0500

    message

--- todolist.py
+++ todolist.py
@@ -22,7 +22,7 @@ def save_to_file(tasks):
 def find_tasks(key,index):
   task_list = []
   tasks=load_file()
-  if key.upper() == 'NONE': 
+  if key.upper() == 'SPEC': 
     if index.upper() == 'ALL':
       return tasks
     else: 
@@ -30,8 +30,8 @@ def find_tasks(key,index):
       return 'NO KEY OR INDEX'
   elif key in KEYS: 
     for task in tasks:
-      if index == task[key]: task_list.append(task)
-      elif index in task[key] and key in KEYS[4:5]: task_list.append(task)
+      if index.lower() == str(task[key] ).lower(): task_list.append(task)
+      elif index.lower() in str(task[key] ).lower() and key in KEYS[4:5]: task_list.append(task)
       else: 
         print('This index does not exist')
         return 'NO KEY OR INDEX'
@@ -58,10 +58,33 @@ def delete_from_file(key,index):
   save_to_file(tasks)
 
 def update_task_description(id,description):
-  pass
-
-def change_task_status(id,status):
-  pass
+  tasks = load_file()
+  new_task = find_tasks('id',id)
+  if new_task == 'NO KEY OR INDEX':
+    return
+  new_task = new_task[0]
+  new_task['description'] = description
+  new_task['time last updated'] = time.asctime( time.gmtime() )
+  for task in tasks:
+    if task['id'] == new_task['id']: 
+      tasks[tasks.index(task)] = new_task
+  save_to_file(tasks)
+  
+def update_task_status(id,status):
+  tasks = load_file()
+  new_task = find_tasks('id',id)
+  if new_task == 'NO KEY OR INDEX':
+    return
+  if status.upper() not in STATUS:
+    print('This status is not suported')
+    return
+  new_task = new_task[0]
+  new_task['status'] = status.upper()
+  new_task['time last updated'] = time.asctime( time.gmtime() )
+  for task in tasks:
+    if task['id'] == new_task['id']: 
+      tasks[tasks.index(task)] = new_task
+  save_to_file(tasks)
 
 
 
@@ -71,15 +94,18 @@ print(command)
 match command[1].lower():
   case 'add': add_to_file( command[2],command[3] )
   case 'delete': delete_from_file( command[2],command[3] )
+  case 'update': update_task_description( command[2],command[3] )
+  case 'mark': update_task_status( command[2],command[3] )
   case 'info': 
     print('\n Keys: id, title, description, status, time created, time last updated',
+          '\n Special Key: spec | Special Index: all',
           '\n Status: todo, in progress, done',
           '\n\n -COMMANDS-'
           '\n add "title" "description" ',
           '\n delete "key" "index" ',
           '\n update "id" "decription" ',
-          '\n mark ""',
-          '\n list',
+          '\n mark "id" "status" ',
+          '\n list "key" "index" ',
           '\n')
   case other: print('This command does not exist')
 
